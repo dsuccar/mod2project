b4ec8d45467f169e8feb8a62e8a9da416fa2ab6b
tzipz23 <tomazyeanderson@gmail.com>
2020-04-14 11:42:31 -0400

    latest

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 resources  :dogs
 resources :users
-# get '/home', to: apps#home
-get '/about', to: "apps#about"
+get '/home', to: "application#home"
+get '/about', to: "application#about"
 # get '/login', to: sessions#login
 # post '/login', to :sessions#process_login
 end
