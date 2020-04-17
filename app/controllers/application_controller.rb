2fe148d691292be95c14194f978e6d363ef8ae1f
tzipz23 <tomazyeanderson@gmail.com>
2020-04-17 11:44:43 -0400

    new

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,14 +18,11 @@
 
     def logged_in?
      
-<<<<<<< HEAD
-=======
 
         !!current_user
     end
 
     def authorized
->>>>>>> 0016d2932f81fbb9df23b3c0a270bed3f3f1a1a7
 
         !!current_user
     end
@@ -46,11 +43,6 @@
         
     end
 
-        if !logged_in?
-            flash["error"] = "You must be logged in"
-            redirect_to login_path
-        end
-    end
 
     def admin?
         
--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,13 +1,8 @@
 class DogsController < ApplicationController
     # before_action :authorized, except: [:index, :show,:edit, :update]
     before_action :logged_in?, except: [:index, :show]
-<<<<<<< HEAD
-    before_action :authorized, except: [:index, :show]
-    before_action :admin?
-=======
   
     before_action :admin?, except: [:index, :show]
->>>>>>> 0016d2932f81fbb9df23b3c0a270bed3f3f1a1a7
     
 
     def index
@@ -29,11 +24,7 @@ class DogsController < ApplicationController
         @dog = Dog.create(dog_params)
             if  @dog.valid?
                 @dog.save
-<<<<<<< HEAD
-                redirect_to dog_path(@dog)
-=======
                 redirect_to dogs_path
->>>>>>> 0016d2932f81fbb9df23b3c0a270bed3f3f1a1a7
             else 
                     render :new
              end
@@ -63,7 +54,7 @@ class DogsController < ApplicationController
     end 
 
     def dog_params
-        params.require(:dog).permit(:name)
+        params.require(:dog).permit(:name, :img)
     end
 
 
